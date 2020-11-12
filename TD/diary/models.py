922926a1938e2df8212474cb6c201227b98bd6c1
detalikota <detalikota@gmail.com>
2020-11-12 11:51:51 +0300

    Just some database stuff

--- TD/diary/models.py
+++ TD/diary/models.py
@@ -3,3 +3,13 @@ from django.db import models
 class Trade(models.Model):
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    pub_date = models.DateTimeField('date published')
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=1000)
+    votes = models.IntegerField(default=0)
---
+++ tests/test.py
@@ -0,0 +1,3 @@
+name = input ("Enter your name: ")
+age = int(input ("Enter your age: "))
+print ("Hello, mister "+name+" you will turn 100 years old in "+str(100-age)+" years")
