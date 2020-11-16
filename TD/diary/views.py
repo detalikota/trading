993f6c7bef215252669899a71eaad6be1ce828ed
detalikota <detalikota@gmail.com>
2020-11-16 14:49:38 +0300

    yep

--- TD/diary/views.py
+++ TD/diary/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Works")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
@@ -13,3 +16,6 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def test(request, question_id):
+    return HttpResponse("I am testing \"%s\"" % question_id)
